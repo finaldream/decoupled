f87b014ded8e2314dad687420e369b5c4aaa158c
Hiep Thai <thaichanhiep@gmail.com>
2018-08-21 13:51:43 +0700

    Add related posts into handleRouteWithSlug

--- packages/decoupled/src/router/default-routes.ts
+++ packages/decoupled/src/router/default-routes.ts
@@ -42,7 +42,28 @@ const handleRouteWithSlug = async (site: Site, req: ServerRequest) => {
         return apiFetch(site, { type, params });
     }
 
-    return site.cachedFetch({ type, params });
+    const result = await site.cachedFetch({ type, params });
+    const template = get(result, 'meta.template', 'index');
+    const pageType = get(result, 'posts.0.type');
+    const pageId = get(result, 'posts.0.id');
+    const categoryId = get(result, 'posts.0.categories.0.term_id', pageId);
+    const recosType = site.config.get(`content.recos.${pageType}`, 'taboola');
+
+    if (template !== 'index' && recosType === 'related-posts' && categoryId) {
+        const res = await site.cachedFetch({
+            type: 'related-posts',
+            params: { term_id : categoryId },
+        });
+
+        const post = get(result, 'posts.0');
+        const relatedPosts = get(res, 'posts', []);
+
+        return Object.assign({}, result, { posts: [
+            Object.assign({}, post, { relatedPosts }),
+        ] });
+    }
+
+    return result;
 };
 
 const handleCacheInvalidate = async (site: Site, req: ServerRequest) => {
@@ -100,7 +121,7 @@ const handlePreviewRequest = async (site: Site, req: ServerRequest) => {
 
 export const DefaultRoutes = [
     new Route({
-        handler: handlePreviewRequest,
+        handler: [handleMenus, handlePreviewRequest],
         method: 'GET',
         route: '/preview(/)',
     }),

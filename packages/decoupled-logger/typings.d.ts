aeb5f22c969626c630ab61b763ddc27034ec037b
Hao Nguyen <hao@zigvy.com>
2018-08-30 01:50:37 +0700

    DC-11 use symlink for build script and config typescript

--- packages/decoupled-logger/typings.d.ts
+++ packages/decoupled-logger/typings.d.ts
@@ -1,12 +1,1 @@
-declare module "*.json" {
-    const value: any;
-    export default value;
-}
-
-declare type AnyObject = {
-    [key: string]: any;
-}
-
-declare interface GenericFunction<A, R> {
-    (arg: A): R;
-}
+../decoupled/typings.d.ts

9a412b198cc9d088721657b4990053c21a24ae57
olivererdmann <o.erdmann@finaldream.de>
2018-09-08 15:32:29 +0200

    Update renderer.test.ts

--- packages/decoupled/src/renderer/renderer.test.ts
+++ packages/decoupled/src/renderer/renderer.test.ts
@@ -3,13 +3,13 @@ import { Renderer } from './renderer';
 import { Site } from '../site/site';
 
 describe('Renderer', () => {
-    test('Initial should success with correct direction', () => {
+    test('Initial should succeed with correct directory', () => {
         const rootPath = path.resolve('src/fixtures/config/');
         const site = new Site('default', rootPath);
         const render = new Renderer(site, require('decoupled-renderer-react'));
         expect(render).toHaveProperty('site');
     });
-    test('Initial should fail with wrong direction', () => {
+    test('Initial should fail with wrong directory', () => {
         try {
             const site = new Site('default', '');
             const render = new Renderer(site, require('decoupled-renderer-react'));    

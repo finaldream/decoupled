23477a921f553916999f516071c7c69e6cee1f9c
olivererdmann <o.erdmann@finaldream.de>
2018-11-07 12:43:58 +0100

    decoupled-logger: change config-files from symlink to real files

--- packages/decoupled-logger/typings.d.ts
+++ packages/decoupled-logger/typings.d.ts
@@ -1,1 +1,12 @@
-../decoupled/typings.d.ts
+declare module "*.json" {
+    const value: any;
+    export default value;
+}
+
+declare type AnyObject = {
+    [key: string]: any;
+}
+
+declare interface GenericFunction<A, R> {
+    (arg: A): R;
+}

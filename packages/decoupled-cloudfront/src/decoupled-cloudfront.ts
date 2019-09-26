c3013f9fa46d8036b0760acdfe8a7dbefba3e3f5
Carlos González Rico <cgonzalezr@gmail.com>
2019-09-26 20:01:54 +0200

    logic and definitions

---
+++ packages/decoupled-cloudfront/src/decoupled-cloudfront.ts
@@ -0,0 +1,66 @@
+/**
+ * AWS CloudFront
+ */
+
+import AWS from 'aws-sdk';
+import uuid from 'uuid/v4';
+
+export default class CloudFront {
+
+    private site: any;
+    private logger: any;
+    private backendNotify: any;
+    private distributionId: string;
+    private client: AWS.CloudFront;
+
+    constructor(site: any) {
+        this.site = site;
+        this.logger = site.logger;
+        this.backendNotify = site.backendNotify;
+
+
+        const { accessKey, secretKey } = site.config.get('services.amazon.awsConfig', {});
+        this.distributionId = site.config.get('services.amazon.cloudfront.distributionId');
+        if (this.distributionId && accessKey && secretKey) {
+            this.logger.debug('CloudFront Invalidation support enabled.');
+        } else {
+            this.logger.debug('CloudFront Invalidation support disabled.');
+            return;
+        }
+
+        this.client = new AWS.CloudFront({
+            accessKeyId: accessKey,
+            secretAccessKey: secretKey,
+        });
+    }
+
+    public invalidate(paths: string[]) {
+
+        if (!this.client) {
+            return;
+        }
+
+        this.logger.debug('CloudFront.invalidate:', paths);
+
+        const params: AWS.CloudFront.CreateInvalidationRequest = {
+            DistributionId: this.distributionId,
+            InvalidationBatch: {
+                CallerReference: `cf-invalidate-${uuid()}`,
+                Paths: {
+                    Quantity: paths.length,
+                    Items: paths,
+                },
+            },
+        };
+
+        this.client.createInvalidation(params, (err, data) => {
+            if (err) {
+                this.logger.error('CloudFront.invalidate:', err);
+                this.backendNotify.sendNotification(err, ['Cache', 'CloudFront', 'Error']);
+            } else {
+                this.logger.debug('CloudFront.invalidate:', data);
+                this.backendNotify.sendNotification(data, ['Cache', 'CloudFront']);
+            }
+        });
+    }
+}

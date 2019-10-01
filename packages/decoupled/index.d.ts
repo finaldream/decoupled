0e4cbf519ae657ea550b0fc6f895e13731e5b847
Carlos González Rico <cgonzalezr@gmail.com>
2019-10-01 13:28:57 +0200

    change logger export

--- packages/decoupled/index.d.ts
+++ packages/decoupled/index.d.ts
@@ -1,6 +1,6 @@
 export { Site } from './dist/site/site';
 export * from './dist/site/site-server';
-export { logger } from './dist/logger/default-logger';
+export { Logger } from 'decoupled-logger';
 export { BackendNotify } from './dist/services/backend-notify';
 
 export as namespace Decoupled;

aca6c20c0676018f0136caa696c41401445b556e
olivererdmann <o.erdmann@finaldream.de>
2019-10-03 12:22:48 +0200

    Export decoupled site-server module

--- packages/decoupled/index.d.ts
+++ packages/decoupled/index.d.ts
@@ -1,5 +1,6 @@
 export { Site } from './lib/site/site';
 export * from './lib/site/site-server';
+export * from './lib/server';
 export { Logger } from 'decoupled-logger';
 export { BackendNotify } from './lib/services/backend-notify';
 

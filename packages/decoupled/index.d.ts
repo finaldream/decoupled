00a6f4e21d47b25a65e42d52d57a6bd715586529
Carlos González Rico <cgonzalezr@gmail.com>
2019-10-14 21:10:02 +0200

    adding delayedCacheInvalidate export

--- packages/decoupled/index.d.ts
+++ packages/decoupled/index.d.ts
@@ -3,5 +3,6 @@ export * from './lib/site/site-server';
 export * from './lib/server';
 export { Logger } from 'decoupled-logger';
 export { BackendNotify } from './lib/services/backend-notify';
+export { delayedCacheInvalidate } from './lib/cache/delayed-cache-invalidate';
 
 export as namespace Decoupled;

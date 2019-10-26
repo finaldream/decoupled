024fcc390ee39199b7a3684d8c2d2b6b7bbde69e
olivererdmann <o.erdmann@finaldream.de>
2019-10-26 14:26:18 +0200

    chore(types): fully auto-generated types
    remove the manual entry points for type-definitions by moving the
    main-scripts into /src. This allows them to also be covered by the
    build process.

--- packages/decoupled-data-markdown/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib';
-
-export as namespace DecoupledDataMarkdown;
---
+++ packages/decoupled/src/index.ts
@@ -0,0 +1,5 @@
+export * from './site/site';
+export * from './site/site-server';
+export * from './server';
+export * from './logger';
+export * from './services/backend-notify';

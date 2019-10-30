e3337bedece4cd265e8eb653e71e275dd55360a7
Carlos González Rico <cgonzalezr@gmail.com>
2019-10-30 22:24:35 +0100

    removing rubbish

--- packages/decoupled/src/fetch/fetch.ts
+++ packages/decoupled/src/fetch/fetch.ts
@@ -8,9 +8,6 @@ export const fetch = async (site: Site, url: string, init : RequestInit = defaul
 
     const authentication = site.config.get('services.wpapi.authentication');
     if (authentication) {
-        if (!init) {
-
-        }
         if (authentication.username && authentication.password) {
             const encoded = new Buffer(`${authentication.username}:${authentication.password}`).toString('base64');
             Object.assign(init.headers, { Authorization: `Basic ${encoded}` });
